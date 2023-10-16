db63a3e761dca44e651f30bbc0fa2ca6da7a6a57
ADIKIAA <zhekov.nick@yandex.ru>
2023-10-16 18:49:30 +0300

    3. @ModelAttribute. Создание инцидента. [#261013]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -4,15 +4,17 @@ import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.job4j.accidents.service.AccidentService;
 
 @Controller
+@RequestMapping("/")
 @Data
 public class IndexController {
 
     private final AccidentService accidentService;
 
-    @GetMapping("/")
+    @GetMapping("/index")
     public String index(Model model) {
         model.addAttribute("accidents", accidentService.findAll());
         return "index";
